59c79151c72726ddabe2e9cc1195c6d1c59a5fcd
Michele Farneti <michele.farneti@studio.unibo.it>
2025-04-08 17:19:09 +0200

    Fixed ControlUnitConnectionAgent function names

--- temperature-monitoring-subsystem/ControlUnitCoonectionAgent.cpp
+++ temperature-monitoring-subsystem/ControlUnitCoonectionAgent.cpp
@@ -1,16 +1,20 @@
 #include "ControlUnitConnectionAgent.h"
 ControlUnitConnectionAgent::ControlUnitConnectionAgent() {
-    // inizializza tutto qui
+
 }
 
-void ControlUnitConnectionAgent::begin() {
-    // implementazione WiFi/MQTT setup
+void ControlUnitConnectionAgent::sendTemperature(double temperature) {
+
 }
 
 void ControlUnitConnectionAgent::loop() {
-    // chiamata client.loop() ecc.
+
 }
 
-void ControlUnitConnectionAgent::publishMessage(const char* topic, const char* message) {
-    // chiamata client.publish()
+double ControlUnitConnectionAgent::getPeriod() {
+
+}
+
+boolean ControlUnitConnectionAgent::getConnectionStatus() {
+
 }
