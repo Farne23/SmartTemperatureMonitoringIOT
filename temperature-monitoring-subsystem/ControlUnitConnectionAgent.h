a7667f0a1cb3015a2e109c909e1d1d3f12dcf9c2
Michele Farneti <michele.farneti@studio.unibo.it>
2025-04-09 11:10:03 +0200

    Implementation of the setTopics metod

--- temperature-monitoring-subsystem/ControlUnitConnectionAgent.h
+++ temperature-monitoring-subsystem/ControlUnitConnectionAgent.h
@@ -2,13 +2,21 @@
 #define CONTROLUNITINTERFACE
 
 #include "ControlUnitInteface.h"
+#include "MQTTClientConnection.h"
 
 class ControlUnitConnectionAgent : public ControlUnitInteface{
-    ControlUnitInterface(parametri);
-    void sendTemperature(double temperature) override;
-    double getPeriod() override;
-    void loop() override;
-    boolean getConnectionStatus() override;
-}
+    public:
+        ControlUnitInterface(parametri);
+        void sendTemperature(double temperature) override;
+        double getPeriod() override;
+        void loop() override;
+        boolean getConnectionStatus() override;
+        void setTopics(char* temperature_topic, char* connection_topic, char* periods_topic);
+    private:
+        MQTTClientConnection* mqttClient;
+        char* temperature_topic; 
+        char* connection_topic;
+        char* periods_topic;
+    }
 
 #endif
--- temperature-monitoring-subsystem/ControlUnitCoonectionAgent.cpp
+++ temperature-monitoring-subsystem/ControlUnitCoonectionAgent.cpp
@@ -1,6 +1,12 @@
 #include "ControlUnitConnectionAgent.h"
-ControlUnitConnectionAgent::ControlUnitConnectionAgent() {
+ControlUnitConnectionAgent::ControlUnitConnectionAgent(const char* ssid, const char* password, const char* server, const char* username, const char* mqtt_password) {
+    mqttClient = new MQTTClientConnection(ssid, password, server, username, mqtt_password);
+}
 
+void ControlUnitConnectionAgent::setTopics(char* temperature_topic, char* connection_topic, char* periods_topic){
+    char* temperature_topic = temperature_topic; 
+    char* connection_topic = connection_topic;
+    char* periods_topic = periods_topic;
 }
 
 void ControlUnitConnectionAgent::sendTemperature(double temperature) {
